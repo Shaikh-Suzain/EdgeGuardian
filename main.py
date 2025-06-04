11e01fa28fb8eb7323b266d8655bd3bbfd5e572f
Shaikh Suzain <shaikhsuzain111@gmail.com>
2025-06-05 02:09:36 +0530

    Added multiply feature in features.py and updated main.py to test it

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import core
-
+import features
 if __name__ == "__main__":
     core.greet("Shaikh Suzain")
     result = core.add_numbers(10, 5)
     print("The result of addition is:", result)
+    product = features.multiply(4,3)
+    print("The result of multiplication is:", product)
+
