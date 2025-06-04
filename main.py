c0ddfd0f78d11f1fb1c2f1cf77cc02006da08c2f
Shaikh Suzain <shaikhsuzain111@gmail.com>
2025-06-04 21:21:01 +0530

    Added core.py with basic functions and updated .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello from EdgeGuardian!") 
+import core
+
+if __name__ == "__main__":
+    core.greet("Shaikh Suzain")
+    result = core.add_numbers(10, 5)
+    print("The result of addition is:", result)
